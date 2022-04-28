86f8140e00b7c58c9d0d0b0890fd8f36923c382e
androiddevnukus <romankrutya@gmail.com>
2022-04-28 07:10:25 +0500

    first commit

---
+++ app/src/main/java/uz/project/a4photos1word/GameActivity.kt
@@ -0,0 +1,11 @@
+package uz.project.a4photos1word
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
---
+++ app/src/main/java/uz/project/a4photos1word/MainActivity.kt
@@ -0,0 +1,20 @@
+package uz.project.a4photos1word
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<TextView>(R.id.play_btn)
+        button.setOnClickListener {
+            val intent = Intent(this,GameActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
